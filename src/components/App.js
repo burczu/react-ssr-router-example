f12621e4e727974c6c683420cc1435f1f55ae97e
burczu <b.dybowski@gmail.com>
2018-01-15 06:58:41 +0100

    tweaks in App comp.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Route, Link, Switch } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 
-const App = (props) => {
+const App = () => {
   return (
     <div>
       <ul>
